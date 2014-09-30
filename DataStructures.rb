15355e651d9f2027573fbaf20c1f02fb377d3d2f
iheartkode <y2kmark73@gmail.com>
2014-09-29 21:40:25 -0700

    Added data structures

---
+++ DataStructures.rb
@@ -0,0 +1,7 @@
+#Basic Data Structures
+
+#Arrays
+my_array = [1,5,4,6,8,9,10]
+
+#Hashes
+my_hash {:dog => 'Barks'}
--- strings_and_numbers.rb
+++ strings_and_numbers.rb
@@ -10,5 +10,26 @@ name = "Bob"
 puts "Hello #{name}"
 
 
+#String Concatenation
+"Hello" + "World"
+
 #Symbols are  immutable strings
 :name
+
+
+############# Numbers #################
+# Example of integers
+1, 2, 3, 50, 10, 4345098098
+
+# Example of floats
+1.2345, 2345.4267, 98.2234
+
+#nil
+puts "Hello World"
+#returns nil
+
+#Operations
+25 + 5
+2 * 2
+1 / 5
+16 % 4
