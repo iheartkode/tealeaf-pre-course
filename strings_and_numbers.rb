0f3d080950c090a4e7193018b13645dd32587084
iheartkode <y2kmark73@gmail.com>
2014-09-29 21:29:03 -0700

    Added strings_and_numbers.rb

---
+++ strings_and_numbers.rb
@@ -0,0 +1,14 @@
+#Strings
+# Ex. 1: with double quotes
+"The man said, 'Hi there!'"
+
+# Ex 2: with single quotes and escaping
+'The man said, \'Hi there!\''
+
+#With double quotes you can use string interpolation
+name = "Bob"
+puts "Hello #{name}"
+
+
+#Symbols are  immutable strings
+:name
